b325ef0aaf6f83203c9fb86870839f98cfe54186
Polina <92095402+Apollynaria@users.noreply.github.com>
2021-10-12 22:12:26 +0800

    Add files via upload

---
+++ Fizz_Buzz.java
@@ -0,0 +1,20 @@
+package fizz_buzz;
+
+/**
+ *
+ * @author Polly
+ */
+public class Fizz_Buzz {
+
+    public static void main(String[] args) {
+       
+        for(int i = 1; i <= 100; i++){
+            if (i % 3 == 0) System.out.print("Fizz");
+            if (i % 5 == 0) System.out.print("Buzz");      
+            if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
+            System.out.print("\n");
+        }
+        
+    }
+    
+}
